1658fe32be715c016650c7672f6182618c12ad27
Kariqs <kariukibenard189@gmail.com>
2022-06-30 11:26:22 +0300

    eBike

--- app/src/main/java/com/example/ebike/Login.java
+++ app/src/main/java/com/example/ebike/Login.java
@@ -2,14 +2,29 @@ package com.example.ebike;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SignUp = findViewById(R.id.SignUp);
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, com.example.ebike.SignUp.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
