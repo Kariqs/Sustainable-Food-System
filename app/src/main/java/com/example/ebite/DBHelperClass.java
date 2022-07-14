fc135a5bcb4531e6921f6eb3659f6d6859308be5
Kariqs <kariukibenard189@gmail.com>
2022-07-14 10:04:50 +0300

    eBite

---
+++ app/src/main/java/com/example/ebite/DBHelperClass.java
@@ -0,0 +1,66 @@
+package com.example.ebite;
+
+public class DBHelperClass {
+
+    String name,username,email,phonenumber,password,confirmPassword;
+
+    public DBHelperClass() {
+    }
+
+    public DBHelperClass(String name, String username, String email, String phonenumber, String password, String confirmPassword) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
--- app/src/main/java/com/example/ebite/SignUp.java
+++ app/src/main/java/com/example/ebite/SignUp.java
@@ -9,19 +9,62 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
     ImageView logo;
     TextView tv1,tv2;
-    EditText name,username,email,phoneNumber,password,confirmPassword;
+    TextInputLayout name,username,email,phonenumber,password,confirmPassword;
     Button register,haveAccount;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+         //Hooks to xml elements on the sign up activity
         haveAccount = findViewById(R.id.haveAccount);
+        register = findViewById(R.id.register);
+        name = findViewById(R.id.name);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        phonenumber = findViewById(R.id.phonenumber);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmpassword);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            rootNode = FirebaseDatabase.getInstance();
+            reference = rootNode.getReference("users");
+
+            //Get Values entered by the user
+            if (password.getEditText().getText().toString().equals(confirmPassword.getEditText().getText().toString())){
+                String NAME = name.getEditText().getText().toString();
+                String USERNAME = username.getEditText().getText().toString();
+                String EMAIL = email.getEditText().getText().toString();
+                String PHONENUMBER = phonenumber.getEditText().getText().toString();
+                String PASSWORD = password.getEditText().getText().toString();
+                String CONFIRMPASSWORD = confirmPassword.getEditText().getText().toString();
+
+                DBHelperClass dbHelperClass = new DBHelperClass(NAME,USERNAME,EMAIL,PHONENUMBER,PASSWORD,CONFIRMPASSWORD);
+                reference.child(PHONENUMBER).setValue(dbHelperClass);
+            } else {
+                Toast.makeText(SignUp.this, "Please confirm your password to proceed", Toast.LENGTH_SHORT).show();
+            }
+
+
+            }
+        });
 
         haveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +74,6 @@ public class SignUp extends AppCompatActivity {
 
             }
         });
+
     }
 }
