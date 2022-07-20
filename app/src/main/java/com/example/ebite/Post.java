823e6939375f2a7d78fa57969b964561c5191161
Kariqs <kariukibenard189@gmail.com>
2022-07-20 16:21:39 +0300

    eBite

--- app/src/main/java/com/example/ebite/Post.java
+++ app/src/main/java/com/example/ebite/Post.java
@@ -31,7 +31,7 @@ import com.squareup.picasso.Picasso;
 public class Post extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
-    Button chooseImage,uploadImage;
+    Button chooseImage, uploadImage;
     TextInputLayout Description;
     ImageView showImage;
     ProgressBar uploadProgress;
@@ -58,83 +58,85 @@ public class Post extends AppCompatActivity {
         chooseImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             openFileChooser();
+                openFileChooser();
             }
         });
         uploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (UploadTask!= null && UploadTask.isInProgress()){
+                if (UploadTask != null && UploadTask.isInProgress()) {
                     Toast.makeText(Post.this, "Upload in progress.", Toast.LENGTH_SHORT).show();
-                }else{
-             uploadFile();
-               // clear();
+                } else {
+                    uploadFile();
+                    // clear();
                 }
             }
         });
     }
-    private void openFileChooser(){
+
+    private void openFileChooser() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() !=null){
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             imageUri = data.getData();
             Picasso.with(this).load(imageUri).into(showImage);
         }
     }
 
-    private String getFileExtension (Uri uri){
+    private String getFileExtension(Uri uri) {
         ContentResolver contentResolver = getContentResolver();
         MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
-        return  mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 
-    private void uploadFile(){
-        if (showImage!=null){
-          StorageReference fileReference = storageReference.child(System.currentTimeMillis()+"."+getFileExtension(imageUri));
-          UploadTask=fileReference.putFile(imageUri)
-                  .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                      @Override
-                      public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                      Handler handler = new Handler();
-                      handler.postDelayed(new Runnable() {
-                          @Override
-                          public void run() {
-                              uploadProgress.setProgress(0);
-                          }
-                      },500);
-                          Toast.makeText(Post.this, "Upload successful.", Toast.LENGTH_LONG).show();
-                          Upload upload = new Upload(Description.getEditText().getText().toString().trim(),
-                                  taskSnapshot.getUploadSessionUri().toString());
-                          String uploadId = databaseReference.push().getKey();
-                          databaseReference.child(uploadId).setValue(upload);
-                      }
-                  })
-                  .addOnFailureListener(new OnFailureListener() {
-                      @Override
-                      public void onFailure(@NonNull Exception e) {
-                          Toast.makeText(Post.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                      }
-                  })
-                  .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                      @Override
-                      public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                      double progress = (100.0*snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
-                      uploadProgress.setProgress((int) progress);
-                      }
-                  });
-        }else{
+    private void uploadFile() {
+        if (showImage != null) {
+            StorageReference fileReference = storageReference.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            UploadTask = fileReference.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    uploadProgress.setProgress(0);
+                                }
+                            }, 500);
+                            Toast.makeText(Post.this, "Upload successful.", Toast.LENGTH_LONG).show();
+                            Upload upload = new Upload(Description.getEditText().getText().toString().trim(),
+                                    taskSnapshot.getUploadSessionUri().toString());
+                            String uploadId = databaseReference.push().getKey();
+                            databaseReference.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Post.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                            uploadProgress.setProgress((int) progress);
+                        }
+                    });
+        } else {
             Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
         }
 
     }
-    private void clear(){
+
+    private void clear() {
         Description.getEditText().setText("");
         showImage.setImageResource(android.R.color.transparent);
     }
