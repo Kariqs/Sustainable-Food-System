832983a88827c1caed46e2f919445bbb32047b78
Kariqs <kariukibenard189@gmail.com>
2022-07-20 11:44:20 +0300

    eBite

--- app/src/main/java/com/example/ebite/Dashboard.java
+++ app/src/main/java/com/example/ebite/Dashboard.java
@@ -2,13 +2,28 @@ package com.example.ebite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
+
+    Button Post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Post = findViewById(R.id.post_button);
+        Post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Post.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/ebite/Post.java
+++ app/src/main/java/com/example/ebite/Post.java
@@ -1,14 +1,64 @@
 package com.example.ebite;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.squareup.picasso.Picasso;
 
 public class Post extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    Button chooseImage,uploadImage;
+    TextInputLayout Description;
+    ImageView showImage;
+    ProgressBar uploadProgress;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        chooseImage = findViewById(R.id.select_image);
+        uploadImage = findViewById(R.id.upload_post);
+        Description = findViewById(R.id.description);
+        showImage = findViewById(R.id.show_image);
+        uploadProgress = findViewById(R.id.upload_progress);
+
+        chooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             openFileChooser();
+            }
+        });
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() !=null){
+            imageUri = data.getData();
+            Picasso.with(this).load(imageUri).into(showImage);
+        }
     }
 }
