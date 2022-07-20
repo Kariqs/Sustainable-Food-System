0e6ac0d6c1b8529506deba2f1b0ff9180fae6598
Kariqs <kariukibenard189@gmail.com>
2022-07-20 13:24:05 +0300

    eBite

--- app/src/main/java/com/example/ebite/Post.java
+++ app/src/main/java/com/example/ebite/Post.java
@@ -1,17 +1,31 @@
 package com.example.ebite;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 public class Post extends AppCompatActivity {
@@ -23,6 +37,11 @@ public class Post extends AppCompatActivity {
     ProgressBar uploadProgress;
     Uri imageUri;
 
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+
+    StorageTask UploadTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +52,9 @@ public class Post extends AppCompatActivity {
         showImage = findViewById(R.id.show_image);
         uploadProgress = findViewById(R.id.upload_progress);
 
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+
         chooseImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +64,12 @@ public class Post extends AppCompatActivity {
         uploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (UploadTask!= null && UploadTask.isInProgress()){
+                    Toast.makeText(Post.this, "Upload in progress.", Toast.LENGTH_SHORT).show();
+                }else{
+             uploadFile();
+               // clear();
+                }
             }
         });
     }
@@ -60,5 +87,55 @@ public class Post extends AppCompatActivity {
             imageUri = data.getData();
             Picasso.with(this).load(imageUri).into(showImage);
         }
+    }
+
+    private String getFileExtension (Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return  mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadFile(){
+        if (showImage!=null){
+          StorageReference fileReference = storageReference.child(System.currentTimeMillis()+"."+getFileExtension(imageUri));
+          UploadTask=fileReference.putFile(imageUri)
+                  .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                      @Override
+                      public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                      Handler handler = new Handler();
+                      handler.postDelayed(new Runnable() {
+                          @Override
+                          public void run() {
+                              uploadProgress.setProgress(0);
+                          }
+                      },500);
+                          Toast.makeText(Post.this, "Upload successful.", Toast.LENGTH_LONG).show();
+                          Upload upload = new Upload(Description.getEditText().getText().toString().trim(),
+                                  taskSnapshot.getUploadSessionUri().toString());
+                          String uploadId = databaseReference.push().getKey();
+                          databaseReference.child(uploadId).setValue(upload);
+                      }
+                  })
+                  .addOnFailureListener(new OnFailureListener() {
+                      @Override
+                      public void onFailure(@NonNull Exception e) {
+                          Toast.makeText(Post.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                      }
+                  })
+                  .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                      @Override
+                      public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                      double progress = (100.0*snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
+                      uploadProgress.setProgress((int) progress);
+                      }
+                  });
+        }else{
+            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private void clear(){
+        Description.getEditText().setText("");
+        showImage.setImageResource(android.R.color.transparent);
     }
 }
---
+++ app/src/main/java/com/example/ebite/Upload.java
@@ -0,0 +1,34 @@
+package com.example.ebite;
+
+public class Upload {
+    String  description,imageUri;
+
+    public Upload() {
+    }
+
+    public Upload(String description, String imageUri) {
+
+        if (description.trim().equals("")){
+          description = "No description";
+        }
+
+        this.description = description;
+        this.imageUri = imageUri;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+}
