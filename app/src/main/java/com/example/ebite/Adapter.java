5f8505595b9b2b50d33d8bafafa8aeb17f25e322
Kariqs <kariukibenard189@gmail.com>
2022-07-21 10:23:55 +0300

    eBite

---
+++ app/src/main/java/com/example/ebite/Adapter.java
@@ -0,0 +1,59 @@
+package com.example.ebite;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter <Adapter.ImageViewHolder> {
+
+   private Context context;
+   private List<Upload> list;
+
+    public Adapter(Context context, List<Upload> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.show_uploads,parent,false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+      Upload upload = list.get(position);
+      holder.Description.setText(upload.getDescription());
+        Picasso.with(context)
+                .load(upload.getImageUri())
+                .fit()
+                .centerCrop()
+                .into(holder.ShowImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ImageViewHolder extends  RecyclerView.ViewHolder{
+          TextView Description;
+          ImageView ShowImage;
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Description = itemView.findViewById(R.id.description_view);
+            ShowImage = itemView.findViewById(R.id.show_uploads);
+        }
+    }
+}
--- app/src/main/java/com/example/ebite/Dashboard.java
+++ app/src/main/java/com/example/ebite/Dashboard.java
@@ -1,20 +1,63 @@
 package com.example.ebite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
 
     Button Post;
+    RecyclerView recyclerView;
+    Adapter adapter;
+
+    DatabaseReference databaseReference;
+    List <Upload> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+              for (DataSnapshot postSnapshot : snapshot.getChildren()){
+                  Upload upload = postSnapshot.getValue(Upload.class);
+                  list.add(upload);
+              }
+
+              adapter = new Adapter(Dashboard.this,list);
+              recyclerView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Dashboard.this,error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Post = findViewById(R.id.post_button);
         Post.setOnClickListener(new View.OnClickListener() {
